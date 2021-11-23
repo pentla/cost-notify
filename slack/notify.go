02c69f7b19813bf937c5735b22f4734504c1717a
Watanabe Kazuki <watanabekazuki@Watanabes-MacBook-Pro.local>
2021-11-23 15:03:35 +0900

    slackにpostできるように

--- slack/notify.go
+++ slack/notify.go
@@ -1,43 +1,41 @@
 package slack
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"net/url"
 
 	"github.com/pentla/cost-notify/entity"
 	"golang.org/x/xerrors"
 )
 
 type SlackText struct {
-	Text       string `json:"text"`
-	Username   string `json:"username"`
-	Icon_emoji string `json:"icon_emoji"`
-	Icon_url   string `json:"icon_url"`
-	Channel    string `json:"channel"`
+	Text string `json:"text"`
+	// Username   string `json:"username"`
+	// Icon_emoji string `json:"icon_emoji"`
+	// Icon_url   string `json:"icon_url"`
+	// Channel    string `json:"channel"`
 }
 
 func PostBudget(webhookURL string, budget *entity.DailyBudget) error {
-	text := SlackText{
-		Text: fmt.Sprintf("GCPの予算の%d %%に達しました。現在の課金額: %f, 予算: %f", budget.AlertThresholdExceeded, budget.CostAmount, budget.BudgetAmount),
+	payload := SlackText{
+		Text: fmt.Sprintf("GCPの予算の%d %%に達しました。現在の課金額: %.2f, 予算: %.2f", budget.AlertThresholdExceeded, budget.CostAmount, budget.BudgetAmount),
 	}
-	var values url.Values
-	textBytes, _ := json.Marshal(text)
-	err := json.Unmarshal(textBytes, &values)
+	payloadJSON, err := json.Marshal(payload)
 	if err != nil {
-		return xerrors.Errorf("Marshal error: %v", err)
+		return xerrors.Errorf("marshal error: %v", err)
 	}
-	resp, err := http.PostForm(webhookURL, values)
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewReader(payloadJSON))
 	if err != nil {
 		return xerrors.Errorf("Request error: %v", err)
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		bodyBytes, err := io.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			return xerrors.Errorf("Response statuscode is not 200: %s %v", resp.Status, err)
 		}
 		return xerrors.Errorf("Response statuscode is not 200: %s %s", resp.Status, string(bodyBytes))
 	}
